44c1b8ba267786dc7ff6bbdd154f2d79d3d4fa8f
Hoang Pham <lehoangpham1092@gmail.com>
2024-09-22 11:03:24 +0700

    chore: added graphql samples.

--- recipe-api/producer-graphql.js
+++ recipe-api/producer-graphql.js
@@ -2,13 +2,16 @@
 const server = require("fastify")();
 const graphql = require("fastify-gql");
 const fs = require("fs");
+// 1: The schema file is provided to the `graphql` package.
 const schema = fs
   .readFileSync(__dirname + "/../shared/graphql-schema.gql")
   .toString();
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 4000;
 
+// 2: The `resolvers` object tells `graphql` how to build responses.
 const resolvers = {
+  // 3: The `Query` entry represents the top-level query.
   Query: {
     pid: () => process.pid,
     recipe: async (_obj, { id }) => {
@@ -20,6 +23,7 @@ const resolvers = {
       };
     },
   },
+  // 4: The `Recipe` resolver is run when a Recipe is retrieved.
   Recipe: {
     ingredients: async (obj) => {
       return obj.id != 42
@@ -32,7 +36,15 @@ const resolvers = {
   },
 };
 
+// Once the GraphQL code has run the `resolvers` and has received the top-level recipe it expects from the `recipe()`
+// method call, and assuming the client has requested the `ingredients`, it's now ready to call the code to
+// hydrate those ingredient values, which is `resolvers.Recipe.ingredients`.
+// Now the `obj` arg contains information about the parent object `Recipe`.
+// The parent object contains all of the information that was returned from the `recipe()` method call.
+
 server
+  // 5: Fastify uses `server.register()` with the `fastify-gql` package; other frameworks have
+  // their own conventions.
   .register(graphql, { schema, resolvers, graphiql: true })
   .listen(PORT, HOST, (err) => {
     if (err) {
---
+++ web-api/consumer-graphql.js
@@ -0,0 +1,37 @@
+// npm install fastify@3.2 node-fetch@2.6
+
+const server = require("fastify")();
+const fetch = require("node-fetch");
+const HOST = "127.0.0.1";
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || "localhost:4000";
+// 1: Here's a more complex query that accepts arguments (query variables).
+const complex_query = `query kitchenSink ($id:ID) {
+  recipe(id: $id) {
+    id name
+    ingredients {
+      name quantity
+    }
+  }
+  pid
+}`;
+
+server.get("/", async () => {
+  const req = await fetch(`http://${TARGET}/graphql`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // 2: The request body is JSON encapsulating the GraphQL query.
+    body: JSON.stringify({
+      query: complex_query,
+      variables: { id: "42" },
+    }),
+  });
+  return {
+    consumer_pid: process.pid,
+    producer_data: await req.json(),
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Consumer running at http://${HOST}:${PORT}/`);
+});
